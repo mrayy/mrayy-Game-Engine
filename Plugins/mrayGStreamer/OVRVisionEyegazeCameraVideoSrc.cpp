538b4ba1c566fc98115c192848c7aa1f37f21346
yamen-personal <mrayyamen@gmail.com>
2017-09-12 12:42:21 +0900

    Adding OVRVision Eyegaze streamer

---
+++ Plugins/mrayGStreamer/OVRVisionEyegazeCameraVideoSrc.cpp
@@ -0,0 +1,32 @@
+
+
+#include "stdafx.h"
+#include "OVRVisionEyegazeCameraVideoSrc.h"
+
+
+
+namespace mray
+{
+namespace video
+{
+
+OVRVisionEyegazeCameraVideoSrc::OVRVisionEyegazeCameraVideoSrc()
+{
+}
+
+OVRVisionEyegazeCameraVideoSrc::~OVRVisionEyegazeCameraVideoSrc()
+{
+}
+
+std::string OVRVisionEyegazeCameraVideoSrc::_generateFullString()
+{
+
+}
+
+
+
+
+
+
+}
+}
---
+++ Plugins/mrayGStreamer/OVRVisionEyegazeCameraVideoSrc.h
@@ -0,0 +1,29 @@
+
+
+#ifndef __OVRVisionEyegazeCameraVideoSrc__
+#define __OVRVisionEyegazeCameraVideoSrc__
+
+// Created: 2015/12/27
+// Author: MHD Yamen Saraiji
+
+#include "EyegazeCameraVideoSrc.h" 
+
+namespace mray
+{
+namespace video
+{
+
+class OVRVisionEyegazeCameraVideoSrc :public EyegazeCameraVideoSrc
+{
+protected:
+	virtual std::string _generateFullString();
+public:
+	OVRVisionEyegazeCameraVideoSrc();
+	virtual ~OVRVisionEyegazeCameraVideoSrc();
+
+};
+
+}
+}
+
+#endif
